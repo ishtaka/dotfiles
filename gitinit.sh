433617c3752ba178c7d1c17290678a74afe7883b
ahakits <ahakits1987@gmail.com>
2019-02-10 15:22:44 +0900

    add gitinit.sh

---
+++ gitinit.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+    exit 1
+fi
+
+# user info
+git config --global user.name $1
+git config --global user.email $2
+
+# prevent garbled
+git config --global core.quotepath false
+
+# auto lf
+git config --global core.autocrlf input
+
+# color ui
+git config --global color.ui auto
+git config --global color.status auto
+git config --global color.branch auto
+
+# git global ignore
+git config --global core.excludesfile ~/.gitignore_global
+
+# alias
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.ch checkout
+git config --global alias.co commit
+git config --global alias.df diff
+git config --global alias.dfc "diff --cached"
+git config --global alias.openpr \!"f() { hub browse -- \`git log --merges --oneline --reverse --ancestry-path \$1...master | grep 'Merge pull request #' | head -n 1 | cut -f5 -d' ' | sed -e 's%#%pull/%'\`; }; f"
