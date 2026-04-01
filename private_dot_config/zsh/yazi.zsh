b083aa450605744c23047ae552d363f860411c4b
ishtaka <ishtak1987@gmail.com>
2026-04-01 10:51:16 +0900

    add yazi

---
+++ private_dot_config/zsh/yazi.zsh
@@ -0,0 +1,11 @@
+# yazi.zsh - Yazi file manager shell integration
+
+# yy: launch yazi and cd to its last directory on exit
+function yy() {
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+    yazi "$@" --cwd-file="$tmp"
+    if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+        builtin cd -- "$cwd"
+    fi
+    rm -f -- "$tmp"
+}
